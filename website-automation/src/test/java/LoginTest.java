5ce5fa2027c62b1c716fa18d01d872b2f819bccb
Christian <christianib003@gmail.com>
2023-11-20 10:22:11 +0200

    add the website URL to the driver

--- website-automation/src/test/java/LoginTest.java
+++ website-automation/src/test/java/LoginTest.java
@@ -4,11 +4,14 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class LoginTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
 
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+
+        Thread.sleep(2000);
         driver.quit();
     }
 }
