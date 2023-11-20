c061316ebf9d5713a8a9010960b399c86a818740
Christian <christianib003@gmail.com>
2023-11-20 10:18:10 +0200

    initialize webdriver

--- website-automation/src/test/java/LoginTest.java
+++ website-automation/src/test/java/LoginTest.java
@@ -1,3 +1,12 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class LoginTest {
-    
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
 }
