8e8bd0e7df10b9ea26235021aaa75e8e949c12e6
Christian <christianib003@gmail.com>
2023-11-21 09:17:38 +0200

    find the forgot password link using webdriverwait instance

--- website-automation/src/test/java/Authentication/ForgotPassword.java
+++ website-automation/src/test/java/Authentication/ForgotPassword.java
@@ -1,7 +1,14 @@
 package Authentication;
 
+import java.time.Duration;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -10,8 +17,12 @@ public class ForgotPassword {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
-        
+
         driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(3));
+        WebElement forgotPasswordLink = wait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.xpath("//*[@id=\"app\"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[4]/p")));
 
         Thread.sleep(3000);
         driver.quit();
