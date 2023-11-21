4def25d2750933b54f70b09425cf65a000cb0c0e
Christian <christianib003@gmail.com>
2023-11-21 10:08:40 +0200

    remove unnecessary imports

--- website-automation/src/test/java/Authentication/ForgotPassword.java
+++ website-automation/src/test/java/Authentication/ForgotPassword.java
@@ -6,11 +6,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import dev.failsafe.internal.util.Assert;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class ForgotPassword {
