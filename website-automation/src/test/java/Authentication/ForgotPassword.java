fc4f2e4da1d88af81829acc9dd1f88f56156fcb7
Christian <christianib003@gmail.com>
2023-11-21 09:08:04 +0200

    setup driver

--- website-automation/src/test/java/Authentication/ForgotPassword.java
+++ website-automation/src/test/java/Authentication/ForgotPassword.java
@@ -1,5 +1,17 @@
 package Authentication;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class ForgotPassword {
-    
+    public static void main(String[] args) throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+
+        Thread.sleep(3000);
+        driver.quit();
+    }
 }
