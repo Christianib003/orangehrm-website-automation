e19ea7c7dcc07bbfba291cb2a60b9262deceff85
Christian <christianib003@gmail.com>
2023-11-21 09:10:31 +0200

    maximize window

--- website-automation/src/test/java/Authentication/ForgotPassword.java
+++ website-automation/src/test/java/Authentication/ForgotPassword.java
@@ -9,6 +9,8 @@ public class ForgotPassword {
     public static void main(String[] args) throws InterruptedException {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        
         driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
 
         Thread.sleep(3000);
